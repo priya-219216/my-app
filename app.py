4185750fa7d84310d6c875bdefd023c5647a1b98
Priya P <priya.p219216@gmail.com>
2025-12-10 14:47:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 def add_numbers(a, b):
     return a + b
+    
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Division by zero!")
+    return a / b
 
 def main():
     print("sum App")
